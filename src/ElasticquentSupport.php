8b04705dddac04d9be8f9e3acff61345808937b1
Luis Carneiro <eltugis@gmail.com>
2016-08-01 10:32:28 +0100

    Workaround to have Elasticquent on Lumen >5
    This is a workaround to use until the elasticquent package starts supporting Lumen.

--- src/ElasticquentSupport.php
+++ src/ElasticquentSupport.php
@@ -2,14 +2,12 @@
 
 namespace Elasticquent;
 
-use Illuminate\Foundation\Application;
-
 class ElasticquentSupport
 {
     use ElasticquentClientTrait;
 
     public static function isLaravel5()
     {
-        return version_compare(Application::VERSION, '5', '>');
+        return true;
     }
 }
